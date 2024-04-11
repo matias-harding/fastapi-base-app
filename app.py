7b4d465a306c8510c6017e09485af34cbf19556c
Matias Harding <matias.harding@gmail.com>
2024-04-11 04:02:37 -0400

    Added json todo list endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request, Depends, Form, status
 
+from fastapi.responses import JSONResponse
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
@@ -27,6 +28,14 @@ def home(request: Request, db: Session = Depends(get_db)):
   todos = db.query(models.Todo).all()
   return templates.TemplateResponse("base.html", 
                                     {"request": request, "todos": todos})
+@app.get("/api/todos")
+def get_todos(db: Session = Depends(get_db)):
+  todos = db.query(models.Todo).all()
+  todos_data = [todo.__dict__ for todo in todos]
+  # Exclude metadata for serialization
+  todos_data = [{k: v for k, v in todo.items() if k != "_sa_instance_state"} for todo in todos_data]
+  
+  return JSONResponse(status_code=status.HTTP_200_OK, content=todos_data)
 
 @app.post("/add")
 def add_todo(request: Request, title: str = Form(...), db: Session = Depends(get_db)):
@@ -47,7 +56,7 @@ def update_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
   return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
 
 @app.get("/delete/{todo_id}")
-def delete(request: Request, todo_id: int, db: Session = Depends(get_db)):
+def delete_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
   todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
   db.delete(todo)
   db.commit()
